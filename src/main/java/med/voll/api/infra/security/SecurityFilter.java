44a2d74e9705c2b64c4f8a11adb1108def6b243a
GUSTAVO NASCIMENTO PASSOS <gustavo.passos@serpro.gov.br>
2024-04-15 11:23:26 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jdk.swing.interop.SwingInterOpUtils;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenservice;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("chamando o filtro");
+        var tokenJWT = recuperarToken(request);
+        if(tokenJWT != null){
+            var subject = tokenservice.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("LOGADÃO!");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ","");
+        }
+        return null;
+    }
+}
---
+++ src/main/resources/db/migration/V5__alter-table-medicos-add-column-time.sql
@@ -0,0 +1,1 @@
+alter table medicos add time varchar(20) not null;
